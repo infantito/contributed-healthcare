c59e81335e98049c67e79bceb35095161823e5b8
infantito <danielinfantito61@gmail.com>
2021-02-14 18:32:49 -0500

    perf: clear by going to home

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,13 +1,16 @@
-import { Link, useLocation } from 'react-router-dom'
-import { Grid, GridSize, Typography } from '@material-ui/core'
+import { useHistory, useLocation } from 'react-router-dom'
+import { Button, Grid, GridSize, Typography } from '@material-ui/core'
 import { Routes } from 'utils/constants'
 import Family from './family'
 
 import './styles.scss'
 import logo from 'assets/logo.png'
+import { storage } from 'utils'
 
 const Header: React.FC = () => {
   const location = useLocation()
+
+  const history = useHistory()
 
   let sm: GridSize = 6
 
@@ -17,14 +20,22 @@ const Header: React.FC = () => {
     sm = 4
   }
 
+  const handleRedirect = (event: React.MouseEvent) => {
+    storage.remove('current')
+
+    return history.push(Routes.HOME)
+  }
+
   return (
     <Grid item={true} sm={sm}>
       <header className="header">
-        <Link to={Routes.HOME} className="header__logo p-4">
-          <span>
-            <img src={logo} alt="Rimac logo" />
-          </span>
-        </Link>
+        <div className="header__logo p-4">
+          <Button type="button" className="p-0" onClick={handleRedirect}>
+            <span>
+              <img src={logo} alt="Rimac logo" />
+            </span>
+          </Button>
+        </div>
         <Family justImg={justImg} />
         <Typography className="p-4 copyright" variant="overline" component="p">
           © 2021 RIMAC Seguros y Reaseguros.
